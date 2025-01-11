53cacfa3754c15706556bd20063950577f1161f0
Eduardomesut <eduardomesut@gmail.com>
2025-01-11 12:38:04 +0100

    Mostrar ayuda

--- AppFTP/src/appftp/Cliente.java
+++ AppFTP/src/appftp/Cliente.java
@@ -17,7 +17,8 @@ public class Cliente {
         System.out.println("Bienvenido al servicio de transferencia FTP");
         boolean salir = false;
         GestorFTP gestor = new GestorFTP();
-        do {
+        do { 
+            mostrarAyuda();
             try {
                 System.out.println(gestor.conectar("10.0.2.15", 21, "marco", "marco"));
             } catch (IOException ex) {
@@ -32,4 +33,18 @@ public class Cliente {
         
     }
     
+    public static void mostrarAyuda(){
+        System.out.println("AYUDA");
+        System.out.println("-------------------------------------------------------------------------");
+        System.out.println("-Para conectarse a un servidor remoto, comando: connect NombreServidor Usuario/Contraseña");
+        System.out.println("-Para mostrar contenido del directorio de trabajo, comando: List");
+        System.out.println("-Para cambiar el directorio de trabajo, comando: changePath NuevoPAth");
+        System.out.println("-Para descargar fichero delde el servidor a local, comando: down NombreFicheroRemoto");
+        System.out.println("-Para subir fichero desde local al servidor, comando: up NombreFicheroLocal");
+        System.out.println("-Para desconectar de un servidor remoto, comando: Disconnect");
+        System.out.println("-------------------------------------------------------------------------");
+        
+        
+        
+    }
 }
